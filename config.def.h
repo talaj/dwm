6cdfd51c43a13582f455f5feb377b26d3a6f4a69
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2019-11-12 21:18:56 +0100

    Update app name

--- config.def.h
+++ config.def.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Chromium-browser",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
